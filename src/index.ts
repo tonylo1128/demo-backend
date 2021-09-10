df89107e046283f6255aadf60aa6d0ceadd90ee6
Tony <tony.lo@arch.partnership.com>
2021-09-10 12:57:02 +0800

    update: mount volume update

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req: Request, res: Response) => {
   res.json({
-      return_value: "TS App is Running"
+      return_value: "TS App is DAMN HARD!"
     });
 })
 
